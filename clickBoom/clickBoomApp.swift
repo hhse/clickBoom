70e327d7afc530bd602c79f2c1086a7beaad0980
mumu <690481115@qq.com>
2026-01-28 19:51:29 +0800

    Initial Commit

---
+++ clickBoom/clickBoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  clickBoomApp.swift
+//  clickBoom
+//
+//  Created by 木木 on 2026/1/28.
+//
+
+import SwiftUI
+
+@main
+struct clickBoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
